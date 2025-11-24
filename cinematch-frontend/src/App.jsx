f9d997673de1282c9275b3e88a8552fd0906b2a5
Leo <leongeorge2002@gmail.com>
2025-11-24 20:34:48 +0200

    Testing TMDB responce in console log (#50)

--- cinematch-frontend/src/App.jsx
+++ cinematch-frontend/src/App.jsx
@@ -3,12 +3,18 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import ProtectedRoute from "./ProtectedRoute";
 import Navigation from "./components/Navigation";
+import { testTmdb } from "./lib/test_tmdb.js";
+import { useEffect } from "react";
 
 export default function App() {
     const location = useLocation();
 
     const hideNavbarRoutes = ["/login"];
     const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
+    useEffect(() => {
+        testTmdb();
+    }, []);
 
     return (
         <>
