9f57f8102c6c729a3d34969ed1d9ce260eb6b84e
Stathis69 <147056555+Stathis69@users.noreply.github.com>
2025-11-25 17:39:05 +0200

    testing trending in homepage (#52)

--- cinematch-frontend/src/lib/test_tmdb.js
+++ cinematch-frontend/src/lib/test_tmdb.js
@@ -11,20 +11,13 @@ export async function testTmdb() {
 
         if (!res.ok) {
             console.error(" TMDB returned an error:", res.status);
-            return;
+            return null;
         }
 
         const data = await res.json();
-
-        console.log(" TMDB Test Successful!");
-        console.log("------------DATA------------");
-        console.log(data);
-
-        console.log("------ PROTES 5 TAINIES ------");
-        data.results.slice(0, 5).forEach((movie, i) => {
-            console.log(`${i + 1}. ${movie.title} (Rating: ${movie.vote_average})`);
-        });
+        return data.results;
     } catch (err) {
         console.error(" TMDB Test Failed:", err);
+        return null;
     }
 }
